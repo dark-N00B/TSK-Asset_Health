0061fa6b735e7644948d44644f3bfed776c14990
darkNOOB <darkn00b@protonmail.com>
2021-06-30 06:41:17 +0530

    Refinement of the existing binaries

--- FusionChartsProject/manage.py
+++ FusionChartsProject/manage.py
@@ -13,6 +13,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
+            "Try installing Django from Python Packages Menu!"
         ) from exc
     execute_from_command_line(sys.argv)
 
