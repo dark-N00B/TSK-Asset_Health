a360f425936311259743e300e05af4937fa237de
darkNOOB <darkn00b@protonmail.com>
2021-06-30 06:32:25 +0530

    Refactored + minimized warnings

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 
 import collections
 import json
-#import re
+# import re
 
 
 def print_hi(name):
 
     import cx_Oracle
 
-    # dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='orcl')  # if needed, place an 'r' before any parameter in order to address special characters such as '\'.
-    # conn = cx_Oracle.connect(user='system', password='yourdbpassword', dsn=dsn_tns)  # if needed, place an 'r' before any parameter in order to address special characters such as '\'. For example, if your user name contains '\', you'll need to place 'r' before the user name: user=r'User Name'
+    #  dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='orcl')
+    #  if needed, place an 'r' before any parameter in order to address special characters such as '\'.
+    #  conn = cx_Oracle.connect(user='system', password='yourdbpassword', dsn=dsn_tns)
+    #  if needed, place an 'r' before any parameter in order to address special characters such as '\'.
+    #  For example, if your user name contains '\', you'll need to place 'r' before the user name: user=r'User Name'
 
     conn = cx_Oracle.connect('system/n00b12345@//localhost:1521/orcl')  # To handle assignment error of 'conn'
 
@@ -41,7 +44,7 @@ def print_hi(name):
         test = ""
         i = 0
         for key, value in d.items():
-            if (i > 0):
+            if i > 0:
                 test = test + ",\n"
             test = test + "{" + '"label": "{}", "value": "{}"'.format(key, value) + "}"
             i = i + 1
@@ -52,12 +55,13 @@ def print_hi(name):
         f.write('{"data": [' + test + "]}")
         f.close()
 
-        t = open("FusionChartsProject/data.json", "r")
+        # t = open("FusionChartsProject/data.json", "r")
         u = open("FusionChartsProject/test.json", "r")
-        j = json.load(t)
+        # j = json.load(t)
         k = json.load(u)
         print(k)
-        t.close()
+        # t.close()
+        u.close()
 
     finally:
         conn.close()
